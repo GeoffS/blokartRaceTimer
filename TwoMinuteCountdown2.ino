7b02f3dfb4276c11c0924f2831858e0ff52fa4bf
Geoff Sobering <geoff@geoff-s.net>
2016-09-17 17:49:47 -0500

    Added finish LED. Set race-length to 5 minutes.

--- TwoMinuteCountdown2.ino
+++ TwoMinuteCountdown2.ino
@@ -2,30 +2,33 @@ const int RF_D0_PIN = 2;
 const int RF_D1_PIN = 3;
 const int RF_D2_PIN = 4;
 const int RF_D3_PIN = 5;
-
+const int FINISHED_LED_PIN = 6;
+const int SPKR_PIN = 7;
 const int PRE_COUNT_PIN = 8;
 const int WARNING_PIN = 9;
 const int DIAL_UP_PIN = 10;
 const int RACING_PIN = 11;
 const int POST_COUNT_PIN = 12;
-const int SPKR_PIN = 13;
+const int SPKR_LED_PIN = 13;
 
 #define ULONG unsigned long
 
-const ULONG s = 200ul; // ms
+const ULONG vs = 120ul; // ms
+const ULONG s = 250ul; // ms
 const ULONG m = 1000ul; // ms
 const ULONG l = 2000ul; // ms
 
-const ULONG raceTime_min = 2ul;
+const ULONG raceTime_min = 5ul;
 const ULONG preDelay_ms = 5000ul;
 
-const int numStates = 27;
-ULONG startTimes_ms[] = {   0ul,  0ul + s, 1000ul, 1000ul + s, 2000ul, 2000ul + s, 3000ul, 3000ul + s, 4000ul, 4000ul + l, 64000ul, 64000ul + m, 94000ul, 94000ul + m, 119000ul, 119000ul + s, 120000ul, 120000ul + s, 121000ul, 121000ul + s, 122000ul, 122000ul + s, 123000ul, 123000ul + s, 124000ul, 124000ul + l, 124000ul + (raceTime_min * 60000ul)};
-int spkrStates[]      = {HIGH,  LOW, HIGH,    LOW, HIGH,    LOW, HIGH,    LOW, HIGH,    LOW,  HIGH,     LOW,  HIGH,     LOW,   HIGH,      LOW,   HIGH,      LOW,   HIGH,      LOW,   HIGH,      LOW,   HIGH,      LOW,   HIGH,      LOW,                         LOW};
-//int pcpStates[]     = { LOW,  LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,   LOW,     LOW,   LOW,     LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,                         LOW};
-int wpStates[]        = {HIGH, HIGH, HIGH,   HIGH, HIGH,   HIGH, HIGH,   HIGH,  LOW,    LOW,   LOW,     LOW,   LOW,     LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,                         LOW};
-int duStates[]        = { LOW,  LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW, HIGH,   HIGH,  HIGH,    HIGH,  HIGH,    HIGH,   HIGH,     HIGH,   HIGH,     HIGH,   HIGH,     HIGH,   HIGH,     HIGH,   HIGH,     HIGH,   HIGH,      LOW,                         LOW};
-int rpStates[]        = { LOW,  LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,   LOW,     LOW,   LOW,     LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    HIGH,    HIGH,                         LOW};
+const int numStates = 28;
+ULONG startTimes_ms[] = {   0ul,  0ul + s, 1000ul, 1000ul + s, 2000ul, 2000ul + s, 3000ul, 3000ul + s, 4000ul, 4000ul + l, 64000ul, 64000ul + m, 94000ul, 94000ul + m, 119000ul, 119000ul + vs, 120000ul, 120000ul + vs, 121000ul, 121000ul + vs, 122000ul, 122000ul + vs, 123000ul, 123000ul + vs, 124000ul, 124000ul + l, 124000ul + (raceTime_min * 60000ul), 124000ul + (raceTime_min * 60000ul) + 60000ul};
+int spkrStates[]      = {HIGH,  LOW, HIGH,    LOW, HIGH,    LOW, HIGH,    LOW, HIGH,    LOW,  HIGH,     LOW,  HIGH,     LOW,   HIGH,      LOW,   HIGH,      LOW,   HIGH,      LOW,   HIGH,      LOW,   HIGH,      LOW,   HIGH,      LOW,  LOW, LOW};
+//int pcpStates[]     = { LOW,  LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,   LOW,     LOW,   LOW,     LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,  LOW, LOW};
+int wpStates[]        = {HIGH, HIGH, HIGH,   HIGH, HIGH,   HIGH, HIGH,   HIGH,  LOW,    LOW,   LOW,     LOW,   LOW,     LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,  LOW, LOW};
+int duStates[]        = { LOW,  LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW, HIGH,   HIGH,  HIGH,    HIGH,  HIGH,    HIGH,   HIGH,     HIGH,   HIGH,     HIGH,   HIGH,     HIGH,   HIGH,     HIGH,   HIGH,     HIGH,   HIGH,      LOW,  LOW, LOW};
+int rpStates[]        = { LOW,  LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,   LOW,     LOW,   LOW,     LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,   HIGH,     HIGH,  LOW, LOW};
+int fpStates[]        = { HIGH,  HIGH,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,  LOW,    LOW,   LOW,     LOW,   LOW,     LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW,    LOW,      LOW, HIGH, LOW};
 
 int currStateCounter = 0;
 ULONG zeroTime_ms;
@@ -47,6 +50,8 @@ void setup()
   pinMode(DIAL_UP_PIN, OUTPUT);
   pinMode(RACING_PIN, OUTPUT);
   pinMode(POST_COUNT_PIN, OUTPUT);
+  pinMode(FINISHED_LED_PIN, OUTPUT);
+  pinMode(SPKR_LED_PIN, OUTPUT);
   pinMode(SPKR_PIN, OUTPUT);
 
   digitalWrite(PRE_COUNT_PIN, LOW);
@@ -54,6 +59,8 @@ void setup()
   digitalWrite(DIAL_UP_PIN, LOW);
   digitalWrite(RACING_PIN, LOW);
   digitalWrite(POST_COUNT_PIN, LOW);
+  digitalWrite(FINISHED_LED_PIN, LOW);
+  digitalWrite(SPKR_LED_PIN, LOW);
   digitalWrite(SPKR_PIN, LOW);
 
   zeroTime_ms = millis();
@@ -71,6 +78,8 @@ void loop()
     digitalWrite(DIAL_UP_PIN, LOW);
     digitalWrite(RACING_PIN, LOW);
     digitalWrite(POST_COUNT_PIN, LOW);
+    digitalWrite(FINISHED_LED_PIN, LOW);
+    digitalWrite(SPKR_LED_PIN, LOW);
     digitalWrite(SPKR_PIN, LOW);
   }
   if (stopped)
@@ -94,12 +103,14 @@ void loop()
       ULONG currTime_ms = millis() - zeroTime_ms;
       if (currTime_ms >= (startTimes_ms[currStateCounter] + preDelay_ms))
       {
+        digitalWrite(SPKR_LED_PIN, spkrStates[currStateCounter]);
         digitalWrite(SPKR_PIN, spkrStates[currStateCounter]);
 
         digitalWrite(PRE_COUNT_PIN, LOW);
         digitalWrite(WARNING_PIN, wpStates[currStateCounter]);
         digitalWrite(DIAL_UP_PIN, duStates[currStateCounter]);
         digitalWrite(RACING_PIN, rpStates[currStateCounter]);
+        digitalWrite(FINISHED_LED_PIN, fpStates[currStateCounter]);
         currStateCounter++;
       }
     }
