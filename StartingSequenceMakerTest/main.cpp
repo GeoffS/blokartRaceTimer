73a3c718b5310aeb58e70d7fa79fa73bbf206a4f
gsobering <gsobering@ametek.com>
2016-10-02 16:41:53 -0500

    Refactored test into more modular form in preparation for more sequences.

--- StartingSequenceMakerTest/main.cpp
+++ StartingSequenceMakerTest/main.cpp
@@ -5,55 +5,27 @@
 
 #define MAX_NUM_STEPS 100
 
-int main(int argc, char **argv)
+bool checkStates(int expectedNumStates, int numStates,
+					ULONG expectedStartTimes_ms[], ULONG startTimes_ms[],
+					bool expectedSpkrStates[], bool spkrStates[],
+					bool expectedFpStates[], bool fpStates[])
 {
-	const ULONG vs = 120ul; // ms
-	const ULONG s = 250ul; // ms
-	const ULONG m = 1000ul; // ms
-	const ULONG l = 2000ul; // ms
-	ULONG raceTime_min = 5ul;
-	const int numStatesOld = 28;
-	ULONG startTimesOld_ms[] = {   0ul,  0ul + s, 1000ul, 1000ul + s, 2000ul, 2000ul + s, 3000ul, 3000ul + s, 4000ul, 4000ul + l, 64000ul, 64000ul + m, 94000ul, 94000ul + m, 119000ul, 119000ul + vs, 120000ul, 120000ul + vs, 121000ul, 121000ul + vs, 122000ul, 122000ul + vs, 123000ul, 123000ul + vs, 124000ul, 124000ul + l, 124000ul + (raceTime_min * 60000ul), 124000ul + (raceTime_min * 60000ul) + 60000ul};
-	bool spkrStatesOld[]     = {true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, false, false};
-	bool fpStatesOld[]       = { false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false, true,  false};
-
-	ULONG startTimes_ms[MAX_NUM_STEPS];
-	bool spkrStates[MAX_NUM_STEPS];
-	bool fpStates[MAX_NUM_STEPS];
-	printf("Hello StartingSequenceMaker\n");
-	StartingSequenceMaker ssm(startTimes_ms, spkrStates, fpStates, MAX_NUM_STEPS);
-
-	ssm.initShort(1);
-	ssm.addShort(1);
-	ssm.addShort(1);
-	ssm.addShort(1);
-	ssm.addLong(60);
-	ssm.addMedium(30);
-	ssm.addMedium(25);
-	ssm.addPip(1);
-	ssm.addPip(1);
-	ssm.addPip(1);
-	ssm.addPip(1);
-	ssm.addPip(1);
-	ssm.addLong(raceTime_min * ssm.SEC_PER_MIN);
-	ssm.addRaceEnd(60);
-
 	bool failure = false;
 	
 	printf("====================\nCheck the number of states:\n-----------\n");
-	printf("numStatesOld = %i\n", numStatesOld);
-	printf("ssm.getNumStates() = %i\n", ssm.getNumStates());
-	if(numStatesOld != ssm.getNumStates())
+	printf("expectedNumStates = %i\n", expectedNumStates);
+	printf("ssm.getNumStates() = %i\n", numStates);
+	if(expectedNumStates != numStates)
 	{
 		printf("*************** Incorrect numStates!\n");
 		failure = true;
 	}
 	
 	printf("\n====================\nCheck the times:\n-----------\n");
-	for (int i = 0; i < ssm.getNumStates(); i++)
+	for (int i = 0; i < numStates; i++)
 	{
-		int delta_ms = startTimesOld_ms[i] - startTimes_ms[i];
-		printf("%i, %i %i\n", startTimesOld_ms[i], startTimes_ms[i], delta_ms);
+		ULONG delta_ms = expectedStartTimes_ms[i] - startTimes_ms[i];
+		printf("%lu, %lu %lu\n", expectedStartTimes_ms[i], startTimes_ms[i], delta_ms);
 		if(delta_ms != 0)
 		{
 			printf("*************** Incorrect time!\n");
@@ -62,10 +34,10 @@ int main(int argc, char **argv)
 	}
 	
 	printf("\n====================\nCheck the siren states:\n-----------\n");
-	for (int i = 0; i < ssm.getNumStates(); i++)
+	for (int i = 0; i < numStates; i++)
 	{
-		printf("%s, %s\n", spkrStatesOld[i]BOOL2STR, spkrStates[i]BOOL2STR);
-		if(spkrStatesOld[i] != spkrStates[i])
+		printf("%s, %s\n", expectedSpkrStates[i]BOOL2STR, spkrStates[i]BOOL2STR);
+		if(expectedSpkrStates[i] != spkrStates[i])
 		{
 			printf("*************** Incorrect speaker state!\n");
 			failure = true;
@@ -73,16 +45,29 @@ int main(int argc, char **argv)
 	}
 	
 	printf("\n====================\nCheck the main-LEDs states:\n-----------\n");
-	for (int i = 0; i < ssm.getNumStates(); i++)
+	for (int i = 0; i < numStates; i++)
 	{
-		printf("%s, %s\n", fpStatesOld[i]BOOL2STR, fpStates[i]BOOL2STR);
-		if(fpStatesOld[i] != fpStates[i])
+		printf("%s, %s\n", expectedFpStates[i]BOOL2STR, fpStates[i]BOOL2STR);
+		if(expectedFpStates[i] != fpStates[i])
 		{
 			printf("*************** Incorrect main LEDs state!\n");
 			failure = true;
 		}
 	}
 	
+	if(failure)
+	{
+		printf("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+		printf("*****  TEST FAILED  *****\n");
+		printf("--------------------------------------------------------\n");
+	}
+	printf("\n\n");
+	
+	return failure;
+}
+
+void printPassFail(bool failure)
+{
 	printf("\n\n\n");
 	if(failure)
 	{
@@ -102,6 +87,55 @@ int main(int argc, char **argv)
 		printf("   P       A       A         S          S\n");
 		printf("   P       A       A   SSSSSS     SSSSSS\n");
 	}
+}
+
+const ULONG vs = 120ul; // ms
+const ULONG s = 250ul; // ms
+const ULONG m = 1000ul; // ms
+const ULONG l = 2000ul; // ms
+
+bool test2minDU5minRace()
+{
+	ULONG raceTime_min = 5ul;
+	const int numStatesOld = 28;
+	ULONG startTimesOld_ms[] = {   0ul,  0ul + s, 1000ul, 1000ul + s, 2000ul, 2000ul + s, 3000ul, 3000ul + s, 4000ul, 4000ul + l, 64000ul, 64000ul + m, 94000ul, 94000ul + m, 119000ul, 119000ul + vs, 120000ul, 120000ul + vs, 121000ul, 121000ul + vs, 122000ul, 122000ul + vs, 123000ul, 123000ul + vs, 124000ul, 124000ul + l, 124000ul + (raceTime_min * 60000ul), 124000ul + (raceTime_min * 60000ul) + 60000ul};
+	bool spkrStatesOld[]     = {true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, false, false};
+	bool fpStatesOld[]       = { false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false,  false, true,  false};
+
+	ULONG startTimes_ms[MAX_NUM_STEPS];
+	bool spkrStates[MAX_NUM_STEPS];
+	bool fpStates[MAX_NUM_STEPS];
+	StartingSequenceMaker ssm(startTimes_ms, spkrStates, fpStates, MAX_NUM_STEPS);
+
+	ssm.initShort(1);
+	ssm.addShort(1);
+	ssm.addShort(1);
+	ssm.addShort(1);
+	ssm.addLong(60);
+	ssm.addMedium(30);
+	ssm.addMedium(25);
+	ssm.addPip(1);
+	ssm.addPip(1);
+	ssm.addPip(1);
+	ssm.addPip(1);
+	ssm.addPip(1);
+	ssm.addLong(raceTime_min * ssm.SEC_PER_MIN);
+	ssm.addRaceEnd(60);
+
+	return checkStates(numStatesOld, ssm.getNumStates(), 
+	                   startTimesOld_ms, startTimes_ms,
+					   spkrStatesOld, spkrStates,
+					   fpStatesOld, fpStates);
+}
+
+int main(int argc, char **argv)
+{
+	bool failure = false;
+	
+	failure |= test2minDU5minRace();
+	
+	printPassFail(failure);
 
 	return 0;
 }
+
